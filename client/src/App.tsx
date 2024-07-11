ab48575cf52189f7fac63a90b378a9191ca1304f
laveesh21 <laveeshtomar.royalking@gmail.com>
2024-07-11 18:24:32 +0530

    Navbar added

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,22 @@
-import { useState } from 'react'
 import './App.css'
+import Navbar from './components/Navbar';
+import About from './pages/About';
+import Home from './pages/Home'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Profile from './pages/Profile';
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      
-    </>
+    <Router>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>om
+        <Route path='about' element={<About/>}/>
+        <Route path='profile' element={<Profile/>}/>
+      </Routes>
+    </Router>
   )
 }
 
---
+++ client/src/components/Navbar.tsx
@@ -0,0 +1,33 @@
+import { Link } from 'react-router-dom';
+import ApexLogo from '../assets/ApexDevsLogo.png'
+
+function Navbar() {
+  return (
+    <div className="w-full h-16 bg-zinc-950 flex justify-between items-center p-4 border-b border-gray-700">
+      <div className="flex items-center space-x-2" id="logo">
+        <img src={ApexLogo} alt="Logo" className="h-8 w-8"/>
+        <div className="text-2xl text-white font-bold">APEX</div>
+        <div className="text-2xl text-green-500 font-bold">DEVS</div>
+      </div>
+      <div className="" id="navbar">
+        <ul className="flex gap-6 text-white">
+          <li className="hover:text-green-500 transition duration-300">
+            <Link to="/">Home</Link>
+          </li>
+          <li className="hover:text-green-500 transition duration-300">
+            <Link to="/about">About</Link>
+          </li>
+        </ul>
+      </div>
+      <div className="" id="profile">
+        <Link to="/profile">
+          <div className="text-white hover:text-green-500 transition duration-300">
+            Profile
+          </div>
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
