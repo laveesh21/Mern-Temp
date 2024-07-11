12f77c30ff6a0dc0a02af828d49a828efde993e7
laveesh21 <laveeshtomar.royalking@gmail.com>
2024-07-11 23:58:31 +0530

    Updated ProjectCard

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -4,6 +4,7 @@ import ApexLogo from '../assets/ApexDevsLogo.png'
 function Navbar() {
   return (
     <div className="w-full h-12 bg-zinc-950 flex justify-between items-center p-4 border-b border-gray-700">
+      
       <div className="flex items-center space-x-2" id="logo">
         <img src={ApexLogo} alt="Logo" className="h-6 w-6"/>
         <div className="text-2xl text-white font-bold">APEX</div>
--- client/src/components/ProjectCard.tsx
+++ client/src/components/ProjectCard.tsx
@@ -1,11 +1,26 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from "react";
+import BookmarkHollow from "../assets/bx-bookmark.svg";
+import BookmarkFill from "../assets/bxs-bookmark.svg";
 
 const data = {
   name: "Project Management Website",
-  dev: "superapex",
-  about: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi, odio? Tenetur veniam eius neque itaque facilis natus sapiente cum facere temporibus nemo. Fugiat ab dolorum ea voluptatum magni. Dolores, atque ex! Ducimus.",
+  dev: "SuperKnox",
+  about:
+    "Lorem ipsum dolor sit amet consectetur ad, atque ex!  dolor sit amet consectetur ad, atque  Ducimus.",
   image: "logo",
-  tags: ['HTML', 'CSS', 'JavaScript', 'Next.js', "Solid", 'React', 'Kotlin', 'Next.js', "Solid", 'React', 'Kotlin'],
+  tags: [
+    "HTML",
+    "CSS",
+    "JavaScript",
+    "Next.js",
+    "Solid",
+    "React",
+    "Kotlin",
+    "Next.js",
+    "Solid",
+    "React",
+    "Kotlin",
+  ],
   date: "23 Nov 2025",
   likes: "12",
   dislikes: "3",
@@ -13,6 +28,7 @@ const data = {
 
 function ProjectCard() {
   const listRef = useRef(null);
+  const [isBookmarked, setIsBookmarked] = useState(false);
 
   const scrollLeft = () => {
     if (listRef.current) {
@@ -26,30 +42,51 @@ function ProjectCard() {
     }
   };
 
+  const toggleBookmark = () => {
+    setIsBookmarked((prev) => !prev); // Toggle bookmark state
+  };
+
   return (
-    <div>
-      <div className='border border-gray-700 w-96 h-96 relative'>
-        <img src="" alt="" className='bg-red-50 w-full h-60'/>
-        <div className='p-2'>
-          <p className='text-emerald-50 mb-'>{data.name}</p>
-          <div className='h-16 line-clamp-3'>
-            <p className='text-sm text-gray-400'>{data.about}</p>
-          </div>
-          <div className='h-px bg-gray-700'/>
-          <div className='mt-3 relative flex items-center'>
-            <button className='absolute left-0 top-0 bottom-0 bg-gray-200 opacity-0 w-6' onClick={scrollLeft}></button>
-            <ul ref={listRef} className='flex gap-2 overflow-hidden'>
-              {data.tags.map((tag, index) => (
-                <li key={index} className='text-xs px-1 text-green-500 border border-green-500 rounded-sm'>
-                  {tag}
-                </li>
-              ))}
-            </ul>
-            <button className='absolute right-0 top-0 bottom-0 bg-gray-200 opacity-0 w-6' onClick={scrollRight}></button>
+    <>
+      <div className="border border-gray-700 w-96 relative rounded-sm m-2">
+        {/* DEVELOPER */}
+        <div className="p-2 \ flex justify-between">
+          <p className="font-bold text-sm text-blue-600 ">{data.dev}</p>
+          <div className="focus:outline-none" onClick={toggleBookmark}>
+            {isBookmarked ? (
+              <img src={BookmarkFill} alt="" className="h-5 w-5 " />
+            ) : (
+              <img src={BookmarkHollow} alt="" className="h-5 w-5" />
+            )}
           </div>
         </div>
+
+        {/* IMAGE */}
+        <img src="" alt="" className="bg-zinc-500 w-full h-60" />
+
+        {/* PROJECT DETAILS */}
+        <div className="p-2">
+          <p className="text-emerald-50 mb-">{data.name}</p>
+          <div className=" line-clamp-3">
+            <p className="text-sm text-gray-400">{data.about}</p>
+          </div>
+
+          <div className="h-px mt-1 bg-gray-700" />
+
+          {/* TAG LIST */}
+          <div className="mt-2 relative flex items-center">
+            <button className="absolute left-0 top-0 bottom-0 bg-gray-200 opacity-0 w-6" onClick={scrollLeft}></button>
+            <ul ref={listRef} className="flex gap-2 overflow-hidden">
+              {data.tags.map((tag, index) => (
+                <li key={index} className="text-xs px-1 text-green-500 border border-green-500">{tag}</li>
+              ))}
+            </ul>
+            <button className="absolute right-0 top-0 bottom-0 bg-gray-200 opacity-0 w-6" onClick={scrollRight}></button>
+          </div>
+        
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
